7957f2af61248e3d3e15eebe4b1261b5af9ad0ad
Charles Stover <github@charlesstover.com>
2019-03-18 12:31:05 -0700

    create-provider

--- src/helpers/create-provider.test.tsx
+++ src/helpers/create-provider.test.tsx
@@ -1,80 +1,83 @@
 import { expect } from 'chai';
-import { Component } from 'react';
-import createProvider from './create-provider';
+import React from 'react';
+import createProvider, { ReactNProvider } from './create-provider';
+
+type RemoveAddedCallback = () => boolean;
 
 describe('createProvider', () => {
+
+  let Provider: ReactNProvider<{}>;
+  beforeEach(() => {
+    Provider = createProvider();
+  });
+
   it('should create a React Component', () => {
-    const Provider = createProvider();
-    expect(Provider).to.be.instanceOf(Component);
+    expect(Provider).to.be.instanceOf(React.Component);
+  });
+
+  describe('static methods', () => {
+
+    describe('addCallback', () => {
+
+      it('should exist', () => {
+        expect(Provider.addCallback).to.be.a('function');
+        expect(Provider.addCallback.length).to.equal(1);
+      });
+
+      it('should return a remove callback function', () => {
+        const removeCallback: RemoveAddedCallback =
+          Provider.addCallback(() => {});
+        expect(removeCallback).to.be.a('function');
+        expect(removeCallback.length).to.equal(0);
+        expect(removeCallback()).to.equal(true);
+      });
+    });
+
+    it('addReducer', () => {
+      expect(Provider.addReducer).to.exist;
+      expect(Provider.addReducer.length).to.equal(1);
+    });
   });
 });
 
 /*
-import React from 'react';
-import Context from '../context';
-import GlobalStateManager from '../global-state-manager';
-import addReducer from './add-reducer';
-import setGlobal from './set-global';
-import useGlobal from './use-global';
-import withGlobal from './with-global';
+static addReducer(name, reducer) {
+  return addReducer(globalStateManager, name, reducer);
+}
 
-export default function createProvider(newGlobal = null) {
-  const globalStateManager = new GlobalStateManager();
-  const result = globalStateManager.set(newGlobal);
+static getGlobal() {
+  return globalStateManager.state;
+}
 
-  class ReactNProvider extends React.Component {
+static get global() {
+  return globalStateManager.state;
+}
 
-    static addCallback(f) {
-      return globalStateManager.addCallback(f);
-    }
+static removeCallback(callback) {
+  return globalStateManager.removeCallback(callback);
+}
 
-    static addReducer(name, reducer) {
-      return addReducer(globalStateManager, name, reducer);
-    }
+static resetGlobal() {
+  return globalStateManager.reset();
+}
 
-    static getGlobal() {
-      return globalStateManager.state;
-    }
+static setGlobal(newGlobal, callback = null) {
+  return setGlobal(globalStateManager, newGlobal, callback);
+}
 
-    static get global() {
-      return globalStateManager.state;
-    }
+static useGlobal(property, setterOnly = false) {
+  return useGlobal(globalStateManager, property, setterOnly);
+}
 
-    static removeCallback(callback) {
-      return globalStateManager.removeCallback(callback);
-    }
+static withGlobal(getter = global => global, setter = () => null) {
+  return withGlobal(globalStateManager, getter, setter);
+}
 
-    static resetGlobal() {
-      return globalStateManager.reset();
-    }
-
-    static setGlobal(newGlobal, callback = null) {
-      return setGlobal(globalStateManager, newGlobal, callback);
-    }
-
-    static useGlobal(property, setterOnly = false) {
-      return useGlobal(globalStateManager, property, setterOnly);
-    }
-
-    static withGlobal(getter = global => global, setter = () => null) {
-      return withGlobal(globalStateManager, getter, setter);
-    }
-
-    render() {
-      return (
-        <Context.Provider value={globalStateManager}>
-          {this.props.children}
-        </Context.Provider>
-      );
-    }
-  }
-
-  // If the state was set asynchronously, return the Provider asynchronously.
-  if (result instanceof Promise) {
-    return result.then(() => ReactNProvider);
-  }
-
-  // Return the Provider synchronously.
-  return ReactNProvider;
+render() {
+  return (
+    <Context.Provider value={globalStateManager}>
+      {this.props.children}
+    </Context.Provider>
+  );
 }
 */
