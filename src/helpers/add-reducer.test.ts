ef133e6286da37d81a99b803f79f92778f32adde
Charles Stover <github@charlesstover.com>
2019-03-21 11:51:17 -0700

    addReducer, setGlobal tests

--- src/helpers/add-reducer.test.ts
+++ src/helpers/add-reducer.test.ts
@@ -1,27 +1,69 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import GlobalStateManager from '../global-state-manager';
 import addReducer from './add-reducer';
+
+type BooleanFunction = () => boolean;
 
 const REDUCER = (_globalState, _one, _two, _three): void => { };
 const REDUCER_NAME = 'reducerName';
 
-describe('Helper Functions', () => {
-  describe('addReducer', () => {
+describe('addReducer', () => {
 
-    it('should add a reducer', () => {
-      const globalStateManager = new GlobalStateManager();
-      addReducer(globalStateManager, REDUCER_NAME, () => {});
-      expect(globalStateManager.hasReducer(REDUCER_NAME));
+  let globalStateManager: GlobalStateManager<{}>;
+  let spy: sinon.SinonSpy;
+  beforeEach(() => {
+    globalStateManager = new GlobalStateManager<{}>();
+  });
+
+  it('should be a function', () => {
+    expect(addReducer).to.be.a('function');
+  });
+
+  it('should accept 3 parameters', () => {
+    expect(addReducer.length).to.equal(3);
+  });
+
+  before(() => {
+    spy = sinon.spy(GlobalStateManager.prototype, 'addReducer');
+  });
+  after(() => {
+    spy.restore();
+  });
+  it(
+    'should call GlobalStateManager.addReducer with correct parameters',
+    () => {
+      addReducer(globalStateManager, REDUCER_NAME, REDUCER);
+      expect(spy.calledOnceWithExactly(REDUCER_NAME, REDUCER)).to.equal(true);
+    }
+  );
+
+  describe('returned remove reducer function', () => {
+
+    let removeReducer: () => boolean;
+    beforeEach(() => {
+      removeReducer = addReducer(globalStateManager, REDUCER_NAME, REDUCER);
     });
 
-    // GlobalReducers currently have a length of 0 because they use
-    //   `...args: any[]` as their parameter definition.
-    // TODO: Some sort of localReducer.bind(null, globalState)
-    it.skip('should make it a global reducer', () => {
-      const globalStateManager = new GlobalStateManager();
-      addReducer(globalStateManager, REDUCER_NAME, REDUCER);
-      expect(globalStateManager.getReducer(REDUCER_NAME).length)
-        .to.equal(REDUCER.length - 1);
+    it('should be a function', () => {
+      expect(removeReducer).to.be.a('function');
+    });
+
+    it('should not accept parameters', () => {
+      expect(removeReducer.length).to.equal(0);
+    });
+
+    let spy: sinon.SinonSpy;
+    before(() => {
+      spy = sinon.spy(GlobalStateManager.prototype, 'removeReducer');
+    });
+    after(() => {
+      spy.restore();
+    });
+    it('should call GlobalStateManager.removeReducer', () => {
+      removeReducer();
+      expect(spy.calledOnceWithExactly(REDUCER_NAME)).to.equal(true);
     });
   });
+
 });
