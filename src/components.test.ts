1bad1d4ff3cef01a2073c7e4eb4c3678a55c50a6
Charles Stover <github@charlesstover.com>
2019-03-21 15:27:40 -0700

    placeholder tests

--- src/components.test.ts
+++ src/components.test.ts
@@ -1,3 +1,15 @@
-describe.skip('Components', () => {
+import {
+  ReactNComponent,
+  ReactNComponentClass,
+  ReactNPureComponent,
+} from './components';
 
+describe('Components', () => {
+
+  const testComponent = (_Super: ReactNComponentClass) => (): void => {
+    it.skip('should do something');
+  };
+
+  describe('ReactNComponent', testComponent(ReactNComponent));
+  describe('ReactNPureComponent', testComponent(ReactNPureComponent));
 });
--- src/helpers/use-global.test.ts
+++ src/helpers/use-global.test.ts
@@ -1,3 +1,3 @@
-describe.skip('useGlobal', () => {
-
+describe('useGlobal', () => {
+  it.skip('should do something');
 });
