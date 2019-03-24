0cfaabd597b56a99a9a881faff355611be737271
Charles Stover <github@charlesstover.com>
2019-03-24 13:17:31 -0700

    reducer change

--- src/global-state-manager.ts
+++ src/global-state-manager.ts
@@ -1,11 +1,22 @@
 import Callback from './typings/callback';
-import {
-  LocalReducer, GlobalReducer, Reducers, RemoveAddedReducer
+import Reducer, {
+  Dispatcher,
+  Dispatchers,
+  Reducers,
 } from './typings/reducer';
 import objectGetListener from './utils/object-get-listener';
 import Transaction from './utils/transaction';
 
 
+
+// Additional Reducers cannot maintain their argument types, as they don't
+//   exist until runtime.
+interface AdditionalDispatchers<GS> {
+  [name: string]: Dispatcher<Reducer<GS, any>>;
+}
+interface AdditionalReducers<GS> {
+  [name: string]: Reducer<GS, any>;
+}
 
 // AsynchronousNewGlobalState is an interface so that NewGlobalState does not
 //   circularly reference itself.
@@ -16,6 +27,14 @@ interface AsynchronousNewGlobalState<Shape>
 interface FunctionalNewGlobalState<Shape> {
   (globalState: Shape): NewGlobalState<Shape>;
 }
+
+/*
+interface GlobalStateManagerClass {
+  new <GS, R>(initialGlobalState: GS, initialReducers: R): GlobalStateManager<GS, R>;
+  new <GS>(initialGlobalState: GS): GlobalStateManager<GS, {}>;
+  new (): GlobalStateManager<{}, {}>;
+}
+*/
 
 export type NewGlobalState<Shape> =
   AsynchronousNewGlobalState<Shape> |
@@ -28,35 +47,67 @@ export type PropertyListener = () => void;
 
 type RemoveAddedCallback = () => boolean;
 
+type RemoveAddedReducer = () => boolean;
+
 type SynchronousNewGlobalState<Shape> = null | Partial<Shape> | void;
 
 
 
+const copyObject = <Shape>(obj: Shape): Shape =>
+  Object.assign(
+    Object.create(null),
+    obj,
+  );
+
 const INVALID_NEW_GLOBAL_STATE: Error = new Error(
-  'Global state must be a function, null, object, or Promise.'
+  'ReactN global state must be a function, null, object, or Promise.',
 );
 
 const MAX_SAFE_INTEGER = 9007199254740990;
 
+const MISSING_TRANSACTION = new Error(
+  'ReactN transaction missing during transaction process.',
+);
 
 
-export default class GlobalStateManager<GS extends {} = {}> {
 
-  private _callbacks: Set<Callback<GS>> = new Set();
+export default class GlobalStateManager<
+  GS extends {} = {},
+  R extends {} = {},
+> {
+
+  private _callbacks: Set<Callback<GS>> = new Set<Callback<GS>>();
+  private _initialReducers: R;
   private _initialState: GS;
-  private _propertyListeners: Map<keyof GS, Set<PropertyListener>> = new Map();
-  private _reducers: Map<string, GlobalReducer<GS>> = new Map();
-  private _state: GS;
-  private _transactionId: number = 0;
-  private _transactions: Map<number, Transaction<GS>> = new Map();
+  private _propertyListeners: Map<keyof GS, Set<PropertyListener>> =
+    new Map<keyof GS, Set<PropertyListener>>();
 
-  public constructor(initialState: GS = Object.create(null)) {
-    this._initialState = Object.assign(
-      Object.create(null),
-      initialState,
-    );
+  // @ts-ignore: Property '_reducers' has no initializer and is not
+  //   definitely assigned in the constructor.
+  private _reducers: R & AdditionalReducers<GS>;
+
+  // @ts-ignore: Property '_state' has no initializer and is not definitely
+  //   assigned in the constructor.
+  private _state: GS;
+
+  // @ts-ignore: Property '_transactionId' has no initializer and is not
+  //   definitely assigned in the constructor.
+  private _transactionId: number;
+  private _transactions: Map<number, Transaction<GS>> =
+    new Map<number, Transaction<GS>>();
+
+
+
+  public constructor(
+    initialState: GS = Object.create(null),
+    initialReducers: R = Object.create(null),
+  ) {
+    this._initialReducers = copyObject(initialReducers);
+    this._initialState = copyObject(initialState);
     this.reset();
   }
+
+
 
   public addCallback(callback: Callback<GS>): RemoveAddedCallback {
     this._callbacks.add(callback);
@@ -72,19 +123,22 @@ export default class GlobalStateManager<GS extends {} = {}> {
 
     // If property listeners already exist for this property,
     //   add this one to the set.
-    if (this._propertyListeners.has(property)) {
-      this._propertyListeners.get(property).add(propertyListener);
+    if (this.propertyListeners.has(property)) {
+      this.propertyListeners.get(property).add(propertyListener);
     }
 
     // If property listeners don't already exist for this property,
     //   create a set of property listeners that includes this one.
     else {
-      this._propertyListeners.set(property, new Set([ propertyListener ]));
+      this.propertyListeners.set(property, new Set([ propertyListener ]));
     }
   }
 
-  public addReducer(name: string, localReducer: LocalReducer<GS>): RemoveAddedReducer {
-    this._reducers.set(name, this.createReducer(localReducer));
+  public addReducer<A extends any[] = any[]>(
+    name: string,
+    reducer: Reducer<GS, A>,
+  ): RemoveAddedReducer {
+    this._reducers[name] = reducer;
     return (): boolean =>
       this.removeReducer(name);
   }
@@ -98,6 +152,11 @@ export default class GlobalStateManager<GS extends {} = {}> {
 
   // Commit a transaction.
   public commit(transactionId: number): void {
+
+    if (!this.hasTransaction(transactionId)) {
+      throw MISSING_TRANSACTION;
+    }
+
     const transaction: Transaction<GS> = this._transactions.get(transactionId);
 
     // Delete state properties.
@@ -126,46 +185,62 @@ export default class GlobalStateManager<GS extends {} = {}> {
     }
   }
 
-  public createReducer(localReducer: LocalReducer<GS>): GlobalReducer<GS> {
-    return (...args: any[]): Promise<GS> =>
+  public createDispatcher<A extends any[] = []>(
+    reducer: Reducer<GS, A>,
+  ): Dispatcher<Reducer<GS, A>> {
+    return (...args: A): Promise<GS> =>
       this.set(
-        localReducer(this.state, ...args),
+        reducer(this.state, ...args),
       );
   }
 
-  public getReducer(reducer: string): GlobalReducer<GS> {
-    return this._reducers.get(reducer);
+  public get dispatchers(): Dispatchers<GS, R> & AdditionalDispatchers<GS> {
+    return Object.entries(this.reducers).reduce(
+      (dispatchers, [ reducerName, reducer]) => {
+        dispatchers[reducerName] = this.createDispatcher(reducer);
+        return dispatchers;
+      },
+      Object.create(null),
+    );
   }
 
-  public get reducers(): Reducers<GS> {
-    const reducers = Object.create(null);
-    for (const [ name, reducer ] of this._reducers.entries()) {
-      reducers[name] = reducer;
-    }
-    return reducers;
+  public getReducer(name: string): null | Reducer<GS> {
+    return this._reducers[name] || null;
   }
 
-  // Share whether the global state has a property listener.
-  // Used in unit testing to prove whether component unmounting has occurred
-  //   successfully.
-  /*
-  hasPropertyListener(property: PropertyListener | string): boolean {
-    if (typeof property === 'string') {
-      return this._propertyListeners.has(property);
-    }
-    for (const propertyListeners of this._propertyListeners.values()) {
+  // Determine whether the global state manager has a callback.
+  // Used in unit testing to track callback addition and removal.
+  public hasCallback(callback: Callback<GS>): boolean {
+    return this._callbacks.has(callback);
+  }
+
+  // Determine whether the global state manager has a property listener.
+  // Used in unit testing to track property listener addition and removal.
+  hasPropertyListener(pl: PropertyListener): boolean {
+    for (const propertyListeners of this.propertyListeners.values()) {
       for (const propertyListener of propertyListeners) {
-        if (propertyListener === property) {
+        if (propertyListener === pl) {
           return true;
         }
       }
     }
     return false;
   }
-  */
 
-  public hasReducer(reducer: string): boolean {
-    return this._reducers.has(reducer);
+  public hasReducer(name: string): boolean {
+    return Object.prototype.hasOwnProperty.call(this._reducers, name);
+  }
+
+  public hasTransaction(id: number): boolean {
+    return this._transactions.has(id);
+  }
+
+  get propertyListeners(): Map<keyof GS, Set<PropertyListener>> {
+    return this._propertyListeners;
+  }
+
+  public get reducers(): R & AdditionalReducers<GS> {
+    return copyObject(this._reducers);
   }
 
   public removeCallback(callback: Callback<GS>): boolean {
@@ -177,7 +252,7 @@ export default class GlobalStateManager<GS extends {} = {}> {
     let removed = false;
 
     // Remove this property listener from the global state.
-    for (const propertyListeners of this._propertyListeners.values()) {
+    for (const propertyListeners of this.propertyListeners.values()) {
       removed = removed || propertyListeners.delete(propertyListener);
     }
 
@@ -190,18 +265,19 @@ export default class GlobalStateManager<GS extends {} = {}> {
   }
 
   public removeReducer(reducer: string): boolean {
-    return this._reducers.delete(reducer);
+    if (this.hasReducer(reducer)) {
+      delete this._reducers[reducer];
+      return true;
+    }
+    return false;
   }
 
   // Reset the global state.
   public reset(): void {
     this._callbacks.clear();
     this._propertyListeners.clear();
-    this._reducers.clear();
-    this._state = Object.assign(
-      Object.create(null),
-      this._initialState,
-    );
+    this._reducers = copyObject(this._initialReducers);
+    this._state = copyObject(this._initialState);
     this._transactionId = 0;
     this._transactions.clear();
   }
@@ -237,11 +313,11 @@ export default class GlobalStateManager<GS extends {} = {}> {
   }
 
   // Set the state's property-value pairs via an object.
-  public setObject(obj: Partial<GS>): Promise<GS> {
+  public setObject<O extends Partial<GS> = Partial<GS>>(obj: O): Promise<GS> {
     const transactionId = this.beginTransaction();
     const properties: (keyof GS)[] = Object.keys(obj) as (keyof GS)[];
     for (const property of properties) {
-      const value = obj[property];
+      const value: GS[keyof GS] = obj[property] as GS[keyof GS];
       this.setProperty(property, value, transactionId);
     }
     this.commit(transactionId);
@@ -265,17 +341,12 @@ export default class GlobalStateManager<GS extends {} = {}> {
     transactionId: number,
   ): number {
 
-    // Silently ignore state properties that share names with reducers.
-    // This can occur if you spread global state with reducers.
-    // newGlobal = { ...globalWithReducers, newProperty: 'new value' }
-    if (
-      typeof property === 'string' &&
-      this.hasReducer(property)
-    ) {
-      return transactionId;
+    if (!this.hasTransaction(transactionId)) {
+      throw MISSING_TRANSACTION;
     }
 
     const transaction: Transaction<GS> = this._transactions.get(transactionId);
+
     if (typeof value === 'undefined') {
       transaction.deleteProperty(property);
     }
@@ -283,10 +354,8 @@ export default class GlobalStateManager<GS extends {} = {}> {
       transaction.setProperty(property, value);
     }
 
-    const propertyListeners: Set<PropertyListener> =
-      this._propertyListeners.get(property);
-    if (propertyListeners) {
-      for (const propertyListener of propertyListeners) {
+    if (this.propertyListeners.has(property)) {
+      for (const propertyListener of this.propertyListeners.get(property)) {
         transaction.addPropertyListener(propertyListener);
       }
     }
@@ -299,16 +368,13 @@ export default class GlobalStateManager<GS extends {} = {}> {
     // When this._state is read, execute the listener.
     return objectGetListener(
       this._state,
-      property => {
+      (property: keyof GS) => {
         this.addPropertyListener(property, propertyListener);
       }
     );
   }
 
-  public get state(): GS {
-    return Object.assign(
-      Object.create(null),
-      this._state
-    );
+  public get state(): Readonly<GS> {
+    return copyObject(this._state);
   }
 };
--- src/typings/reducer.ts
+++ src/typings/reducer.ts
@@ -1,11 +1,35 @@
 import { NewGlobalState } from '../global-state-manager';
 
-export type GlobalReducer<GS> = (...args: any[]) => Promise<GS>;
+/*
+export interface Dispatcher<
+  GS extends {},
+  A extends any[] = any[],
+> {
+  (...args: A): Promise<GS>;
+}
+*/
 
-export type LocalReducer<GS> = (globalState: GS, ...args: any[]) => NewGlobalState<GS>;
+export interface Dispatcher<
+  R extends Reducer<any, any>,
+> extends CallableFunction {
+  (...args: ExtractA<R>): Promise<ExtractGS<R>>;
+}
 
-export interface Reducers<GS> {
-  [reducer: string]: GlobalReducer<GS>;
+export type Dispatchers<GS, R extends Reducers<GS>> = {
+  [name in keyof R]: Dispatcher<R[name]>;
 };
 
-export type RemoveAddedReducer = () => boolean;
+type ExtractA<R> = R extends Reducer<infer _GS, infer A> ? A : never;
+
+type ExtractGS<R> = R extends Reducer<infer GS, infer _A> ? GS : never;
+
+export default interface Reducer<
+  GS extends {} = {},
+  A extends any[] = any[],
+> extends CallableFunction {
+  (globalState: GS, ...args: A): NewGlobalState<GS>;
+};
+
+export interface Reducers<GS extends {} = {}> {
+  [name: string]: Reducer<GS>;
+};
