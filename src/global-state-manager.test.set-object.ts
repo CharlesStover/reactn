8b5ac962943719e4d0cfcd09a30b7c8805eb3d80
Charles Stover <github@charlesstover.com>
2019-03-26 10:52:53 -0700

    gsm

--- src/global-state-manager.test.set-object.ts
+++ src/global-state-manager.test.set-object.ts
@@ -1,11 +1,22 @@
 import { expect } from 'chai';
 import GlobalStateManager from './global-state-manager';
+import { GS, INITIAL_STATE } from './utils/test/initial';
+import spyOn from './utils/test/spy-on-global-state-manager';
+
+
+
+const STATE_CHANGE: Partial<GS> = {
+  x: true,
+};
+
+
 
 export default (): void => {
 
-  let globalStateManager: GlobalStateManager<{}, {}>;
+  let globalStateManager: GlobalStateManager<GS>;
+  const spy = spyOn('enqueue', 'flush');
   beforeEach((): void => {
-    globalStateManager = new GlobalStateManager<{}, {}>();
+    globalStateManager = new GlobalStateManager<GS>(INITIAL_STATE);
   });
 
   it('should be a function with 1 argument', (): void => {
@@ -13,6 +24,31 @@ export default (): void => {
     expect(globalStateManager.setObject.length).to.equal(1);
   });
 
-  it.skip('should do more', (): void => {
+  it('should call enqueue and flush', async (): Promise<void> => {
+    await globalStateManager.setObject(STATE_CHANGE);
+    expect(spy.enqueue.calledOnceWithExactly('x', STATE_CHANGE.x))
+      .to.equal(true);
+    expect(spy.flush.calledOnceWithExactly()).to.equal(true);
+  });
+
+
+
+  describe('return value', (): void => {
+
+    it('should be a Promise', async (): Promise<void> => {
+      const set: Promise<GS | void> =
+        globalStateManager.setObject(STATE_CHANGE);
+      expect(set).to.be.instanceOf(Promise);
+      await set;
+    });
+
+    it('should resolve to the new global state', async (): Promise<void> => {
+      const set: Promise<GS> = globalStateManager.setObject(STATE_CHANGE);
+      const value = await set;
+      expect(value).to.deep.equal({
+        ...INITIAL_STATE,
+        ...STATE_CHANGE,
+      });
+    });
   });
 };
