c2a256d9b28fc54ce6ce7e9a88fc24e5dbc4ae63
Charles Stover <github@charlesstover.com>
2019-03-31 18:27:23 -0700

    potential fix for interface exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,8 @@
-import React = require('react');
+import React from 'react';
 import { ReactNComponent, ReactNPureComponent } from './components';
 import reactn from './decorator';
 import { NewGlobalState } from './global-state-manager';
-import createProvider from './helpers/create-provider';
+import { ReactNProvider } from './helpers/create-provider';
 import {
   GlobalTuple,
   Setter as UseGlobalSetter,
@@ -19,70 +19,87 @@ import Reducer, { Reducers } from './typings/reducer';
 
 
 
+
 type BooleanFunction = () => boolean;
 
-type Decorator = typeof reactn;
-
-declare namespace ReactN {
-  type ComponentClass = ReactNComponentClass;
-}
-
-declare interface ReactN extends Decorator, TypeOfReact {
+interface ReactN extends TypeOfReact {
   <P extends {} = {}, S extends {} = {}, GS extends {} = {}, R extends {} = {}, SS = any>(
     DecoratedComponent: React.ComponentClass<P, S>,
-  ): ReactNComponentClass<P, S, GS, R, SS>;
+  ): ReactNTypes.ComponentClass<P, S, GS, R, SS>;
+
   addCallback<GS extends {} = {}>(
     callback: Callback<GS>,
   ): BooleanFunction;
+
   addReducer<GS extends {} = {}>(
     name: string,
     reducer: Reducer<GS>,
   ): BooleanFunction;
+
   addReducers<GS extends {} = {}>(
     reducers: Reducers<GS>,
   ): BooleanFunction;
-  Component: typeof ReactNComponent;
-  ComponentClass: ReactNComponentClass;
-  createProvider: typeof createProvider;
+
+  Component: typeof ReactNComponent
+
+  createProvider<GS extends {} = {}, R extends {} = {}>(
+    initialState?: GS,
+    initialReducers?: R,
+  ): ReactNProvider<GS, R>;
+
   default: ReactN;
+
   getGlobal<GS extends {} = {}>(): GS;
+
   PureComponent: typeof ReactNPureComponent;
+
   removeCallback<GS extends {} = {}>(
     callback: Callback<GS>,
   ): boolean;
+
   resetGlobal(): void;
+
   setGlobal<GS extends {} = {}>(
     newGlobalState: NewGlobalState<GS>,
     callback?: Callback<GS>,
   ): Promise<GS>;
+
   useGlobal<GS extends {} = {}>(): GlobalTuple<GS>;
+
   useGlobal<GS extends {}, Property extends keyof GS>(
     property: Property,
     setterOnly?: false,
   ): StateTuple<GS, Property>;
+
   useGlobal<GS extends {}, Property extends keyof GS>(
     property: Property,
     setterOnly: true,
   ): UseGlobalSetter<GS, Property>;
+
   withGlobal<GS extends {} = {}, HP extends {} = {}, LP extends {} = {}>(
     getter?: Getter<GS, HP, LP>,
     setter?: WithGlobalSetter<GS, HP, LP>,
   ): WithGlobal<HP, LP>;
 }
 
-interface ReactNComponentClass<
-  P extends {} = {},
-  S extends {} = {},
-  GS extends {} = {},
-  R extends {} = {},
-  SS = any,
-> extends React.ComponentClass<P, S> {
-  new (props: P, context?: any): ReactNComponent<P, S, GS, R, SS>;
+declare namespace ReactNTypes {
+
+  interface ComponentClass<
+    P extends {} = {},
+    S extends {} = {},
+    GS extends {} = {},
+    R extends {} = {},
+    SS = any
+  > extends React.ComponentClass<P, S> {
+    new (props: P, context?: any): ReactNComponent<P, S, GS, R, SS>;
+  }
+  class ComponentClass { }
+
 }
 
 type TypeOfReact = typeof React;
 
 
 
-export = ReactN;
-export as namespace ReactN;
+declare const _export: ReactN & typeof ReactNTypes;
+export = _export;
