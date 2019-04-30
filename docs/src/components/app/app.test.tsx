4f387e5cff7de92901dc03611a401206bb8dc43f
Charles Stover <github@charlesstover.com>
2019-04-29 20:06:17 -0700

    add ReactN globals to docs <App /> unit test

--- docs/src/components/app/app.test.tsx
+++ docs/src/components/app/app.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import '../../reactn';
 import App from './app';
 
 it('renders without crashing', () => {
