b261dc941f2f2307a4e67b892d2d3003b2140344
Charles Stover <github@charlesstover.com>
2019-05-23 16:48:07 -0700

    adds ReactN DevTools to docs

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'reactn';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'reactn';
+import addReactNDevTools from 'reactn-devtools';
 import App from './components/app';
 import './index.scss';
 import './reactn';
+
+addReactNDevTools();
 
 ReactDOM.render(
   <BrowserRouter>
