f354e288d016192a51f2122eaaa279dd79a8a1c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-10 03:47:49 +0000

    Deploy CharlesStover/reactn to github.com/CharlesStover/reactn.git:gh-pages

---
+++ precache-manifest.d1930f4aa67f7b4c2555d05560b93bc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9f5f0c81e525c2d169cb",
+    "url": "./static/js/main.d37fd155.chunk.js"
+  },
+  {
+    "revision": "7c1058d8010ddb45161f",
+    "url": "./static/js/2.8c796395.chunk.js"
+  },
+  {
+    "revision": "9f5f0c81e525c2d169cb",
+    "url": "./static/css/main.4041be3a.chunk.css"
+  },
+  {
+    "revision": "85ba494096df292dfe44509ef59cc8f3",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3b82e6f35605bfbc6f225464385984c2.js"
+  "./precache-manifest.d1930f4aa67f7b4c2555d05560b93bc4.js"
 );
 
 workbox.clientsClaim();
