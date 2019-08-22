01aa93415f8633fcd13d3ffcc7104b31fbed8419
Charles Stover <github@charlesstover.com>
2019-08-22 12:03:22 -0700

    type Dispatch -> interface Dispatch

--- default.d.ts
+++ default.d.ts
@@ -2,7 +2,7 @@ import DispatchFunction from './types/dispatch-function';
 import Dispatchers from './types/dispatchers';
 import NewGlobalState from './types/new-global-state';
 
-export type Dispatch = DispatchFunction<State> & Dispatchers<State, Reducers>;
+export interface Dispatch extends DispatchFunction<State> & Dispatchers<State, Reducers> { }
 
 export interface Reducers { }
 
